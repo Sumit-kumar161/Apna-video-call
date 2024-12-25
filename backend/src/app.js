88e2879d1a2867e6b723dc3ad54bfff938b15f90
Sumit-kumar161 <sumanparul161@gmail.com>
2024-12-25 18:39:14 +0530

    Fix CORS issue and update backend configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,13 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
-app.use(cors());
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://apnavideofrontend-vjvk.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
